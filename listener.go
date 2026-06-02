883117bd31cceb3738da310120ec583e40c2dc28
adman <adman.else@gmail.com>
2026-06-02 18:38:57 +0200

    Revert "Update `PongDataFunc` to inline type definition instead of `pongDataFunc`"
    This reverts commit 4073917d413b56bc66c8aeadcc0bb1ea5bbaca09.

--- listener.go
+++ listener.go
@@ -76,7 +76,7 @@ type Listener struct {
 
 	// pongDataFunc is a function that returns the data that is sent in an unconnected pong
 	// it will be called if pongData is nil.
-	pongDataFunc atomic.Pointer[func(addr net.Addr) []byte]
+	pongDataFunc atomic.Pointer[pongDataFunc]
 }
 
 // listenerID holds the next ID to use for a Listener.
@@ -179,10 +179,12 @@ func (listener *Listener) PongData(data []byte) {
 	listener.pongData.Store(&data)
 }
 
+type pongDataFunc func(addr net.Addr) []byte
+
 // PongDataFunc sets a function to generate pong data dynamically when responding to an unconnected ping.
 // This function will take priority over the static pong data, unless it is set back to nil. The data
 // returned should not be bigger than math.MaxInt16.
-func (listener *Listener) PongDataFunc(f func(addr net.Addr) []byte) {
+func (listener *Listener) PongDataFunc(f pongDataFunc) {
 	if f == nil {
 		listener.pongDataFunc.Store(nil)
 	} else {
